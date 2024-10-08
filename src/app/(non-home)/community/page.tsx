07f234c47bb24badc8c61fba1ff57e5248d6d3e7
Sahil Gathe <sahilsankur@gmail.com>
2024-10-07 17:11:30 -0700

    feat: Added AILA follow

--- src/app/(non-home)/community/page.tsx
+++ src/app/(non-home)/community/page.tsx
@@ -3,8 +3,13 @@ import './Community.scss';
 export default function Community() {
   return (
     <main className="community_page">
-      <h1>Community</h1>
-      <p>hellow</p>
+      <div className="hero">
+        <h1 className="hero-text">
+          We are the creatives, the affordance‑finders, and the next wave of
+          problem‑solvers – we are designers from UCSD.
+        </h1>
+      </div>
+      <div className="img"></div>
     </main>
   );
 }
